57aebf2cc995e37081d8f410f1f0765a9d33c24f
Eileen Burdekin <eileenburdekin@proton.me>
2025-01-07 18:48:59 -0800

    simplify js, add hover behavior for buttons

--- rock-paper-scissors/game.js
+++ rock-paper-scissors/game.js
@@ -2,16 +2,9 @@ const rockButton = document.querySelector("#rock");
 const paperButton = document.querySelector("#paper");
 const scissorsButton = document.querySelector("#scissors");
 
-const buttons = [rockButton, paperButton, scissorsButton];
-
-buttons.forEach((button) => {
-  button.addEventListener("click", (e) => playRound(e));
-});
-
 const humanViz = document.querySelector("#human-viz");
 const computerViz = document.querySelector("#computer-viz");
 
-const choicesDisplay = document.querySelector("#choices");
 const roundResultDisplay = document.querySelector("#round-result");
 
 const humanScoreDisplay = document.querySelector("#human-score");
@@ -21,6 +14,28 @@ const finalResultDisplay = document.querySelector("#final-result");
 const resetButtonPlaceholder = document.querySelector(
   "#reset-button-placeholder"
 );
+
+const handleButtonClick = (e) => playRound(e.target.id);
+
+const handleKeydown = (e) => {
+  const keyMap = {
+    r: "rock",
+    p: "paper",
+    s: "scissors",
+  };
+  if (keyMap[e.key]) {
+    playRound(keyMap[e.key]);
+  }
+};
+
+const buttons = [rockButton, paperButton, scissorsButton];
+
+// use named functions as callbacks, can't remove if using anon function
+buttons.forEach((button) => {
+  button.addEventListener("click", handleButtonClick);
+});
+
+document.addEventListener("keydown", handleKeydown);
 
 const CHOICES = ["rock", "paper", "scissors"];
 
@@ -32,9 +47,8 @@ const getComputerChoice = () => {
 let computerScore = 0;
 let humanScore = 0;
 
-const playRound = (e) => {
+const playRound = (humanChoice) => {
   const computerChoice = getComputerChoice();
-  const humanChoice = e.target.id;
 
   const winningCombos = {
     rock: "scissors",
@@ -42,52 +56,33 @@ const playRound = (e) => {
     scissors: "paper",
   };
 
-  let rockImage = document.createElement("img");
-  rockImage.src = "./images/rock.png";
-  rockImage.alt = "hand playing rock";
-
-  let paperImage = document.createElement("img");
-  paperImage.src = "./images/paper.png";
-  rockImage.alt = "hand playing paper";
-
-  let scissorsImage = document.createElement("img");
-  scissorsImage.src = "./images/scissors.png";
-  scissorsImage.alt = "hand playing scissors";
-
-  const images = {
-    rock: rockImage,
-    paper: paperImage,
-    scissors: scissorsImage,
+  const createImageElement = (choice) => {
+    const img = document.createElement("img");
+    img.src = `./images/${choice}.png`;
+    img.alt = `hand playing ${choice}`;
+    return img;
   };
 
   humanViz.innerHTML = "";
   computerViz.innerHTML = "";
-  humanViz.append(images[humanChoice].cloneNode());
-  computerViz.append(images[computerChoice].cloneNode());
-
-  choicesDisplay.innerText = `You played ${humanChoice}.
-    Computer played ${computerChoice}`;
+  humanViz.append(createImageElement(humanChoice).cloneNode());
+  computerViz.append(createImageElement(computerChoice).cloneNode());
 
   const getWinner = (computerChoice, humanChoice) => {
-    if (computerChoice === humanChoice) return "draw";
-
-    return winningCombos[computerChoice] === humanChoice
-      ? computerChoice
-      : humanChoice;
+    if (computerChoice === humanChoice) {
+      roundResultDisplay.innerText = "Draw.";
+      return;
+    }
+    if (winningCombos[computerChoice] === humanChoice) {
+      computerScore++;
+      roundResultDisplay.innerText = `You lose this round. ${computerChoice} beats ${humanChoice}.`;
+    } else {
+      humanScore++;
+      roundResultDisplay.innerText = `You win this round! ${humanChoice} beats ${computerChoice}.`;
+    }
   };
 
   const roundWinner = getWinner(computerChoice, humanChoice);
-
-  if (roundWinner === "draw") {
-    roundResultDisplay.innerText = "Draw.";
-  } else {
-    roundWinner === computerChoice ? computerScore++ : humanScore++;
-    roundResultDisplay.innerText = `You ${
-      roundWinner === computerChoice
-        ? `lose this round. ${computerChoice} beats ${humanChoice}.`
-        : `win this round! ${humanChoice} beats ${computerChoice}.`
-    }`;
-  }
 
   computerScoreDisplay.innerText = computerScore;
   humanScoreDisplay.innerText = humanScore;
@@ -96,12 +91,24 @@ const playRound = (e) => {
     finalResultDisplay.innerText = `YOU ${
       humanScore > computerScore ? "WIN" : "LOSE"
     }!`;
-    buttons.forEach((button) => {
-      button.setAttribute("disabled", true);
-    });
-    const resetButton = document.createElement("button");
-    resetButton.innerText = "Play again";
-    resetButton.addEventListener("click", () => window.location.reload(true));
-    resetButtonPlaceholder.append(resetButton);
+    disablePlayButtons();
+    addResetButton();
   }
+};
+
+const disablePlayButtons = () => {
+  buttons.forEach((button) => {
+    button.removeEventListener("click", handleButtonClick);
+    document.removeEventListener("keydown", handleKeydown);
+
+    // remove hover class so transform/cursor change no longer happens
+    button.classList.remove("hover-effect");
+  });
+};
+
+const addResetButton = () => {
+  const resetButton = document.createElement("button");
+  resetButton.innerText = "Play again";
+  resetButton.addEventListener("click", () => window.location.reload(true));
+  resetButtonPlaceholder.append(resetButton);
 };
