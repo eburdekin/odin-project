fc7d65e8080a32896f92597232e8ab87cea0f0d0
Eileen Burdekin <eileenburdekin@proton.me>
2025-02-13 21:45:44 -0800

    change if statements to operatorMap

--- 03-calculator/index.js
+++ 03-calculator/index.js
@@ -1,55 +1,45 @@
-const add = (a, b) => {
-  return a + b;
-};
-
-const subtract = (a, b) => {
-  return a - b;
-};
-
-const multiply = (a, b) => {
-  return a * b;
-};
-
-const divide = (a, b) => {
-  return a / b;
+const operatorMap = {
+  "+": (a, b) => {
+    return a + b;
+  },
+  "-": (a, b) => {
+    return a - b;
+  },
+  "*": (a, b) => {
+    return a * b;
+  },
+  "/": (a, b) => {
+    return a / b;
+  },
 };
 
 const operate = (a, operator, b) => {
-  if (operator === "+") {
-    return add(a, b);
-  }
-  if (operator === "-") {
-    return subtract(a, b);
-  }
-  if (operator === "x") {
-    return multiply(a, b);
-  } else {
-    return divide(a, b);
-  }
+  return operatorMap[operator](a, b);
 };
 
-let a;
-let operator;
-let b;
-
 const container = document.querySelector(".container");
-
-const display = document.createElement("div");
-display.classList.add("display");
-display.innerText = "0";
-container.appendChild(display);
+const display = document.querySelector(".display");
 
 const updateDisplay = (content) => {
   display.innerText = content;
 };
 
-const buttons = [7, 8, 9, "+", 4, 5, 6, "-", 1, 2, 3, "x", "C", 0, "=", "/"];
+const buttons = [7, 8, 9, "+", 4, 5, 6, "-", 1, 2, 3, "*", "C", 0, "=", "/"];
+
+let a = "";
+let operator = "";
+let b = "";
+// let isResultDisplay = false;
+
+// const handleButtonClick = (e) => {
+//   const button = e.target.innerText;
+// };
 
 let buttonClickCount = 0;
 
 const handleButtonClick = (e) => {
   const button = e.target.innerText;
-  if (!isNaN(parseInt(button))) {
+  if (!isNaN(button)) {
     updateDisplay(button);
     buttonClickCount++;
     console.log(buttonClickCount);
@@ -79,11 +69,12 @@ buttons.forEach((button) => {
 
 const clearButton = document.querySelector("#C");
 clearButton.addEventListener("click", () => {
+  a = "";
+  operator = "";
+  b = "";
   updateDisplay("0");
   buttonClickCount = 0;
 });
-
-const displayContent = display.innerText;
 
 const equalsButton = document.querySelector("[id='=']");
 equalsButton.addEventListener("click", () => {
